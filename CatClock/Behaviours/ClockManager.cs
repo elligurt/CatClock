4a510a97056c61974c2f4403a23921e5fcd89b2d
elliot! <benjikrolfanpageuwu@gmail.com>
2025-08-17 14:46:37 +0100

    Add project files.

---
+++ CatClock/Behaviours/ClockManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace CatClock.Behaviours
+{
+    public class CatClockManager : MonoBehaviour
+    {
+        public TextMeshPro clockTime;
+        public TextMeshPro clockAMPM;
+
+        public void Initialize(GameObject clockPrefab)
+        {
+            clockTime = clockPrefab.transform.Find("Clock/Time")?.GetComponent<TextMeshPro>();
+            clockAMPM = clockPrefab.transform.Find("Clock/AMPM")?.GetComponent<TextMeshPro>();
+
+            if (clockTime != null && clockAMPM != null)
+            {
+                StartCoroutine(UpdateClockCoroutine());
+            }
+        }
+
+        private IEnumerator UpdateClockCoroutine()
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+
+                if (clockTime != null && clockAMPM != null)
+                {
+                    clockTime.text = now.ToString("hh:mm");
+                    clockAMPM.text = now.ToString("tt");
+                }
+
+                int secondsUntilNextMinute = 60 - now.Second;
+                yield return new WaitForSeconds(secondsUntilNextMinute);
+            }
+        }
+    }
+}
---
+++ CatClock/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using BepInEx;
+using CatClock.Tools;
+using UnityEngine;
+using Utilla;
+using CatClock.Behaviours;
+
+namespace CatClock
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private GameObject clockPrefab;
+        private CatClockManager clockManager;
+
+        void Start() => Utilla.Events.GameInitialized += OnGameInitialized;
+
+        private async void OnGameInitialized(object sender, EventArgs e)
+        {
+            await SetupClock();
+        }
+
+        private async Task SetupClock()
+        {
+            try
+            {
+                clockPrefab = await AssetLoader.LoadAsset<GameObject>("CatClock");
+                if (clockPrefab == null)
+                {
+                    Debug.LogError("[CatClock] Failed to load prefab.");
+                    return;
+                }
+
+                GameObject clockInstance = Instantiate(clockPrefab);
+                clockInstance.SetActive(true);
+
+                clockInstance.transform.position = new Vector3(-65.7865f, 11.7985f, - 79.762f);
+                clockInstance.transform.rotation = Quaternion.Euler(358.6325f, 266.6017f, 359.3513f);
+                clockInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+                clockManager = clockInstance.AddComponent<CatClockManager>();
+                clockManager.Initialize(clockInstance);
+
+                ClockBobber bobber = clockInstance.AddComponent<ClockBobber>();
+                bobber.Initialize(clockInstance);
+
+                Debug.Log("[CatClock] Tick Tock Monke!!");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[CatClock] Error setting up clock: " + ex);
+            }
+        }
+    }
+}
