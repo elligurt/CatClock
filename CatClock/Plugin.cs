a152d00e7215fdc521042fcf57bcaedc643bae4c
elliot! <benjikrolfanpageuwu@gmail.com>
2025-09-05 19:44:08 +0100

    Minor Changes

--- CatClock/Plugin.cs
+++ CatClock/Plugin.cs
@@ -11,10 +11,20 @@ namespace CatClock
     [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
     public class Plugin : BaseUnityPlugin
     {
+        public static Plugin Instance { get; private set; }
+
         private GameObject _clockPrefab;
         private CatClockManager _clockManager;
 
-        void Start() => Utilla.Events.GameInitialized += OnGameInitialized;
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
 
         private async void OnGameInitialized(object sender, EventArgs e)
         {
@@ -31,19 +41,18 @@ namespace CatClock
                     Debug.LogError("[CatClock] Failed to load prefab.");
                     return;
                 }
-
+                
                 GameObject clockInstance = Instantiate(_clockPrefab);
                 clockInstance.SetActive(true);
-
-                clockInstance.transform.position = new Vector3(-65.7865f, 11.7985f, - 79.762f);
+                clockInstance.transform.position = new Vector3(-65.7865f, 11.7985f, -79.762f);
                 clockInstance.transform.rotation = Quaternion.Euler(358.6325f, 266.6017f, 359.3513f);
                 clockInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-
+                
                 _clockManager = clockInstance.AddComponent<CatClockManager>();
                 _clockManager.Initialize(clockInstance);
 
-                ClockBobber bobber = clockInstance.AddComponent<ClockBobber>();
-                bobber.Initialize(clockInstance);
+                ClockActions actions = clockInstance.AddComponent<ClockActions>();
+                actions.Initialize(clockInstance);
 
                 Debug.Log("[CatClock] Tick Tock Monke!!");
             }
