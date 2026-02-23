ec6485be8c3764c511ff1b2ba80e762c14b0c4bd
elliot! <benjikrolfanpageuwu@gmail.com>
2026-02-23 20:26:56 +0000

    Delete CatClock directory

--- CatClock/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace CatClock
-{
-    internal class Constants
-    {
-        public const string GUID = "com.elliot.gorillatag.catclock";
-        public const string Name = "CatClock";
-        public const string Version = "1.0.2";
-    }
-}
